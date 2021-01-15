0b6f22758357515f0a6b160d986794f43aef4fdf
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-16 00:37:11 +0100

    Create WonderfulEnchantment.java

---
+++ WonderfulEnchantments1-16-4/src/main/java/com/wonderfulenchantments/enchantments/WonderfulEnchantment.java
@@ -0,0 +1,32 @@
+package com.wonderfulenchantments.enchantments;
+
+import com.mlib.config.AvailabilityConfig;
+import com.mlib.config.ConfigGroup;
+import com.mlib.enchantments.ExtendedEnchantment;
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.inventory.EquipmentSlotType;
+
+import static com.wonderfulenchantments.WonderfulEnchantments.CURSE_GROUP;
+
+/** Enchantment that automatically adds config to it. */
+public class WonderfulEnchantment extends ExtendedEnchantment {
+	protected final ConfigGroup enchantmentGroup;
+	protected final AvailabilityConfig availabilityConfig;
+
+	protected WonderfulEnchantment( Rarity rarity, EnchantmentType enchantmentType, EquipmentSlotType[] equipmentSlotTypes, String configName ) {
+		super( rarity, enchantmentType, equipmentSlotTypes );
+		String comment = "Makes this enchantment obtainable in survival mode.";
+
+		this.enchantmentGroup = CURSE_GROUP.addGroup( new ConfigGroup( configName, "" ) );
+		this.availabilityConfig = this.enchantmentGroup.addConfig( new AvailabilityConfig( "is_enabled", comment, false, true ) );
+	}
+
+	protected WonderfulEnchantment( Rarity rarity, EnchantmentType enchantmentType, EquipmentSlotType equipmentSlotType, String configName ) {
+		this( rarity, enchantmentType, new EquipmentSlotType[]{ equipmentSlotType }, configName );
+	}
+
+	@Override
+	protected boolean isDisabled() {
+		return this.availabilityConfig.isDisabled();
+	}
+}
