c986d41372b7e5f47f44c508f3aac51a34b51f9c
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-19 09:18:11 +0100

    Updated config file name

--- src/main/java/com/wonderfulenchantments/WonderfulEnchantments.java
+++ src/main/java/com/wonderfulenchantments/WonderfulEnchantments.java
@@ -12,7 +12,7 @@ public class WonderfulEnchantments {
 	public static final String MOD_ID = "wonderful_enchantments";
 	public static final String NAME = "Wonderful Enchantments";
 	public static final String VERSION = "1.0.0";
-	public static final ConfigHandler CONFIG_HANDLER = new ConfigHandler( ModConfig.Type.COMMON, "wonderful-enchantments-new.toml" );
+	public static final ConfigHandler CONFIG_HANDLER = new ConfigHandler( ModConfig.Type.COMMON, "wonderful-enchantments-common.toml" );
 	public static final ConfigGroup ENCHANTMENT_GROUP = CONFIG_HANDLER.addConfigGroup( new ConfigGroup( "Enchantments", "" ) );
 	public static final ConfigGroup CURSE_GROUP = CONFIG_HANDLER.addConfigGroup( new ConfigGroup( "Curses", "" ) );
 
