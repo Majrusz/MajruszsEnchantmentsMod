6981e821663c4a7f73d1d9523b17e44bb6d3427c
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-23 16:51:03 +0100

    Updated modification version

--- src/main/java/com/wonderfulenchantments/WonderfulEnchantments.java
+++ src/main/java/com/wonderfulenchantments/WonderfulEnchantments.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.config.ModConfig;
 public class WonderfulEnchantments {
 	public static final String MOD_ID = "wonderful_enchantments";
 	public static final String NAME = "Wonderful Enchantments";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final ConfigHandler CONFIG_HANDLER = new ConfigHandler( ModConfig.Type.COMMON, "wonderful-enchantments-common.toml" );
 	public static final ConfigGroup ENCHANTMENT_GROUP = CONFIG_HANDLER.addConfigGroup( new ConfigGroup( "Enchantments", "" ) );
 	public static final ConfigGroup CURSE_GROUP = CONFIG_HANDLER.addConfigGroup( new ConfigGroup( "Curses", "" ) );
