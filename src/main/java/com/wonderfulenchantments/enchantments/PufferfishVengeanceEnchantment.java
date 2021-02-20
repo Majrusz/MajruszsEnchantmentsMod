dcf0eba54be09c4b4088536506f75cc6561d35a5
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-02-20 10:49:02 +0100

    Fixed bug with to high duration of Pufferfish enchantment and added config for amplifiers

--- src/main/java/com/wonderfulenchantments/enchantments/PufferfishVengeanceEnchantment.java
+++ src/main/java/com/wonderfulenchantments/enchantments/PufferfishVengeanceEnchantment.java
@@ -2,6 +2,7 @@ package com.wonderfulenchantments.enchantments;
 
 import com.mlib.TimeConverter;
 import com.mlib.config.DurationConfig;
+import com.mlib.config.IntegerConfig;
 import com.mlib.effects.EffectHelper;
 import com.wonderfulenchantments.Instances;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -23,12 +24,19 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber
 public class PufferfishVengeanceEnchantment extends WonderfulEnchantment {
 	protected final DurationConfig durationConfig;
+	protected final IntegerConfig hungerAmplifier, poisonAmplifier, nauseaAmplifier;
 
 	public PufferfishVengeanceEnchantment() {
 		super( Rarity.RARE, EnchantmentType.WEAPON, EquipmentSlotType.MAINHAND, "PufferfishVengeance" );
-		String comment = "Pufferfish negative effects duration per enchantment level. (in seconds)";
-		this.durationConfig = new DurationConfig( "duration", comment, false, 4.0, 1.0, 30.0 );
-		this.enchantmentGroup.addConfig( this.durationConfig );
+		String durationComment = "Pufferfish negative effects duration per enchantment level. (in seconds)";
+		String hungerComment = "Amplifier level of Hunger effect.";
+		String poisonComment = "Amplifier level of Poison effect.";
+		String nauseaComment = "Amplifier level of Nausea effect.";
+		this.durationConfig = new DurationConfig( "duration", durationComment, false, 4.0, 1.0, 30.0 );
+		this.hungerAmplifier = new IntegerConfig( "hunger", hungerComment, false, 2, 0, 10 );
+		this.poisonAmplifier = new IntegerConfig( "poison", poisonComment, false, 3, 0, 10 );
+		this.nauseaAmplifier = new IntegerConfig( "nausea", nauseaComment, false, 0, 0, 10 );
+		this.enchantmentGroup.addConfigs( this.durationConfig, this.hungerAmplifier, this.poisonAmplifier, this.nauseaAmplifier );
 
 		setMaximumEnchantmentLevel( 2 );
 		setDifferenceBetweenMinimumAndMaximum( 20 );
@@ -55,10 +63,9 @@ public class PufferfishVengeanceEnchantment extends WonderfulEnchantment {
 			return;
 
 		LivingEntity target = event.getEntityLiving();
-		int durationInTicks = TimeConverter.secondsToTicks( Instances.PUFFERFISH_VENGEANCE.durationConfig.getDuration() ) + 20;
 		World world = attacker.getEntityWorld();
 
-		applyEffects( target, durationInTicks );
+		Instances.PUFFERFISH_VENGEANCE.applyEffects( target );
 		world.playSound( null, target.getPosX(), target.getPosY(), target.getPosZ(), SoundEvents.ENTITY_PUFFER_FISH_BLOW_OUT, SoundCategory.AMBIENT,
 			1.0f, 1.0f
 		);
@@ -67,12 +74,13 @@ public class PufferfishVengeanceEnchantment extends WonderfulEnchantment {
 	/**
 	 Applying negative effects on given target.
 
-	 @param target          Entity on which negative effects will be applied.
-	 @param durationInTicks Duration how long effect will last. (in ticks)
+	 @param target Entity on which negative effects will be applied.
 	 */
-	protected static void applyEffects( LivingEntity target, int durationInTicks ) {
-		EffectHelper.applyEffectIfPossible( target, Effects.HUNGER, durationInTicks, 2 );
-		EffectHelper.applyEffectIfPossible( target, Effects.POISON, durationInTicks, 3 );
-		EffectHelper.applyEffectIfPossible( target, Effects.NAUSEA, durationInTicks, 0 );
+	protected void applyEffects( LivingEntity target ) {
+		int durationInTicks = this.durationConfig.getDuration() + TimeConverter.secondsToTicks( 1.0 );
+
+		EffectHelper.applyEffectIfPossible( target, Effects.HUNGER, durationInTicks, this.hungerAmplifier.get() );
+		EffectHelper.applyEffectIfPossible( target, Effects.POISON, durationInTicks, this.poisonAmplifier.get() );
+		EffectHelper.applyEffectIfPossible( target, Effects.NAUSEA, durationInTicks, this.nauseaAmplifier.get() );
 	}
 }
