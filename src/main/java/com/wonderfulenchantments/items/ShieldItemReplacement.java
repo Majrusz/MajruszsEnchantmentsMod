cb265a8feca38046082890ac0cc8f2491b910894
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-04-13 19:42:39 +0200

    Update ShieldItemReplacement.java

--- src/main/java/com/wonderfulenchantments/items/ShieldItemReplacement.java
+++ src/main/java/com/wonderfulenchantments/items/ShieldItemReplacement.java
@@ -3,13 +3,14 @@ package com.wonderfulenchantments.items;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ShieldItem;
 
-/** Makes shield enchantable. */
+/** Shield item replacement with visible enchantability. */
 public class ShieldItemReplacement extends ShieldItem {
 	public ShieldItemReplacement() {
 		super( ( new Properties() ).maxDamage( 336 )
 			.group( ItemGroup.COMBAT ) );
 	}
 
+	/** Makes shield enchantable. */
 	@Override
 	public int getItemEnchantability() {
 		return 1;
