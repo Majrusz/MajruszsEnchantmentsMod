b0c4e4d3b134716532072d02b20b2e56fef35563
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-07-27 19:44:22 +0200

    Updated to 1.17.1

--- src/main/java/com/wonderfulenchantments/WonderfulEnchantments.java
+++ src/main/java/com/wonderfulenchantments/WonderfulEnchantments.java
@@ -2,7 +2,7 @@ package com.wonderfulenchantments;
 
 import com.mlib.config.ConfigGroup;
 import com.mlib.config.ConfigHandler;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.config.ModConfig;
 public class WonderfulEnchantments {
 	public static final String MOD_ID = "wonderful_enchantments";
 	public static final String NAME = "Wonderful Enchantments";
-	public static final String VERSION = "1.3.5";
+	public static final String VERSION = "1.4.0";
 	public static final ConfigHandler CONFIG_HANDLER = new ConfigHandler( ModConfig.Type.COMMON, "wonderful-enchantments-common.toml" );
 	public static final ConfigGroup ENCHANTMENT_GROUP = CONFIG_HANDLER.addConfigGroup( new ConfigGroup( "Enchantments", "" ) );
 	public static final ConfigGroup CURSE_GROUP = CONFIG_HANDLER.addConfigGroup( new ConfigGroup( "Curses", "" ) );
