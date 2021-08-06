ab53c3622f432e4a6fc7eb1f8121f35b212705dd
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-08-06 20:51:05 +0200

    Harvester now also tills 3x3 area at maximum enchantment level

--- src/main/java/com/wonderfulenchantments/enchantments/HarvesterEnchantment.java
+++ src/main/java/com/wonderfulenchantments/enchantments/HarvesterEnchantment.java
@@ -2,17 +2,21 @@ package com.wonderfulenchantments.enchantments;
 
 import com.mlib.config.DoubleConfig;
 import com.mlib.config.IntegerConfig;
+import com.mlib.features.FarmlandTiller;
 import com.wonderfulenchantments.Instances;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.enchantment.EnchantmentCategory;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.NetherWartBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -21,6 +25,16 @@ import net.minecraftforge.fml.common.Mod;
 /** Enchantment that automatically replants seeds and gives a chance to improve nearby crops. */
 @Mod.EventBusSubscriber
 public class HarvesterEnchantment extends WonderfulEnchantment {
+	static {
+		FarmlandTiller.registerList.add( new FarmlandTiller.Register() {
+			@Override
+			public boolean shouldBeExecuted( ServerLevel serverLevel, Player player, ItemStack itemStack ) {
+				HarvesterEnchantment harvester = Instances.HARVESTER;
+				return EnchantmentHelper.getItemEnchantmentLevel( harvester, itemStack ) >= harvester.getMaxLevel();
+			}
+		} );
+	}
+
 	public final IntegerConfig range;
 	public final IntegerConfig durabilityPenalty;
 	public final DoubleConfig growChance;
@@ -28,14 +42,19 @@ public class HarvesterEnchantment extends WonderfulEnchantment {
 
 	public HarvesterEnchantment() {
 		super( "harvester", Rarity.UNCOMMON, EnchantmentCategory.DIGGER, EquipmentSlot.MAINHAND, "Harvester" );
+
 		String rangeComment = "Range increase per enchantment level. (per block in x-axis and z-axis)";
-		String durabilityComment = "Penalty for increasing age of nearby crops. (per successful increase)";
-		String growComment = "Chance for increasing age of nearby crops. (calculated for each crop separately)";
-		String wartComment = "Chance for increasing age of nearby Nether Warts. (calculated for each crop separately)";
 		this.range = new IntegerConfig( "range", rangeComment, false, 1, 1, 3 );
+
+		String durabilityComment = "Penalty for increasing age of nearby crops. (per successful increase)";
 		this.durabilityPenalty = new IntegerConfig( "durability_penalty", durabilityComment, false, 1, 1, 10 );
+
+		String growComment = "Chance for increasing age of nearby crops. (calculated for each crop separately)";
 		this.growChance = new DoubleConfig( "grow_chance", growComment, false, 0.04, 0.0, 1.0 );
+
+		String wartComment = "Chance for increasing age of nearby Nether Warts. (calculated for each crop separately)";
 		this.netherWartGrowChance = new DoubleConfig( "nether_wart_grow_chance", wartComment, false, 0.01, 0.0, 1.0 );
+
 		this.enchantmentGroup.addConfigs( this.range, this.durabilityPenalty, this.growChance, this.netherWartGrowChance );
 
 		setMaximumEnchantmentLevel( 3 );
@@ -76,7 +95,9 @@ public class HarvesterEnchantment extends WonderfulEnchantment {
 	}
 
 	/** Harvests nether wart block. */
-	protected static void handleNetherWarts( NetherWartBlock netherWartBlock, Player player, BlockPos position, BlockState blockState, ItemStack itemStack ) {
+	protected static void handleNetherWarts( NetherWartBlock netherWartBlock, Player player, BlockPos position, BlockState blockState,
+		ItemStack itemStack
+	) {
 		int netherWartMaximumAge = 3;
 		if( blockState.getValue( NetherWartBlock.AGE ) < netherWartMaximumAge )
 			return;
