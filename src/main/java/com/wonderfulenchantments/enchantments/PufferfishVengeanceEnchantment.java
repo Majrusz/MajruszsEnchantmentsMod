6d903849e7bff466e7d9f13a6286cc93bea46153
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-09-01 20:50:08 +0200

    Refactoring

--- src/main/java/com/wonderfulenchantments/enchantments/PufferfishVengeanceEnchantment.java
+++ src/main/java/com/wonderfulenchantments/enchantments/PufferfishVengeanceEnchantment.java
@@ -1,5 +1,6 @@
 package com.wonderfulenchantments.enchantments;
 
+import com.mlib.CommonHelper;
 import com.mlib.TimeConverter;
 import com.mlib.config.DurationConfig;
 import com.mlib.config.IntegerConfig;
@@ -28,14 +29,19 @@ public class PufferfishVengeanceEnchantment extends WonderfulEnchantment {
 
 	public PufferfishVengeanceEnchantment() {
 		super( "pufferfish_vengeance", Rarity.RARE, EnchantmentCategory.WEAPON, EquipmentSlot.MAINHAND, "PufferfishVengeance" );
+
 		String durationComment = "Pufferfish negative effects duration per enchantment level. (in seconds)";
-		String hungerComment = "Amplifier level of Hunger effect.";
-		String poisonComment = "Amplifier level of Poison effect.";
-		String nauseaComment = "Amplifier level of Nausea effect.";
 		this.durationConfig = new DurationConfig( "duration", durationComment, false, 4.0, 1.0, 30.0 );
+
+		String hungerComment = "Amplifier level of Hunger effect.";
 		this.hungerAmplifier = new IntegerConfig( "hunger", hungerComment, false, 2, 0, 10 );
+
+		String poisonComment = "Amplifier level of Poison effect.";
 		this.poisonAmplifier = new IntegerConfig( "poison", poisonComment, false, 3, 0, 10 );
+
+		String nauseaComment = "Amplifier level of Nausea effect.";
 		this.nauseaAmplifier = new IntegerConfig( "nausea", nauseaComment, false, 0, 0, 10 );
+
 		this.enchantmentGroup.addConfigs( this.durationConfig, this.hungerAmplifier, this.poisonAmplifier, this.nauseaAmplifier );
 
 		setMaximumEnchantmentLevel( 2 );
@@ -51,22 +57,16 @@ public class PufferfishVengeanceEnchantment extends WonderfulEnchantment {
 	/** Event that checks if attacker has an appropriate enchantment level. */
 	@SubscribeEvent
 	public static void onHit( LivingAttackEvent event ) {
+		PufferfishVengeanceEnchantment pufferfishVengeance = Instances.PUFFERFISH_VENGEANCE;
 		DamageSource damageSource = event.getSource();
-
-		if( !( damageSource.getDirectEntity() instanceof LivingEntity ) )
-			return;
-
-		LivingEntity attacker = ( LivingEntity )damageSource.getDirectEntity();
-		int enchantmentLevel = EnchantmentHelper.getEnchantmentLevel( Instances.PUFFERFISH_VENGEANCE, attacker );
-
-		if( enchantmentLevel <= 0 )
+		LivingEntity attacker = CommonHelper.castIfPossible( LivingEntity.class, damageSource.getDirectEntity() );
+		if( attacker == null || !pufferfishVengeance.hasEnchantment( attacker ) )
 			return;
 
 		LivingEntity target = event.getEntityLiving();
-		Level world = attacker.level;
+		pufferfishVengeance.applyEffects( target );
 
-		Instances.PUFFERFISH_VENGEANCE.applyEffects( target );
-		world.playSound( null, target.getX(), target.getY(), target.getZ(), SoundEvents.PUFFER_FISH_BLOW_OUT, SoundSource.AMBIENT,
+		attacker.level.playSound( null, target.getX(), target.getY(), target.getZ(), SoundEvents.PUFFER_FISH_BLOW_OUT, SoundSource.AMBIENT,
 			1.0f, 1.0f
 		);
 	}
