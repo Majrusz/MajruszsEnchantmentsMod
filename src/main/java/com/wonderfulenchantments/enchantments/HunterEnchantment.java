dda7f0dee6d8346136bab24d61bc47f9a3512f32
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-09-04 08:47:12 +0200

    Refactoring

--- src/main/java/com/wonderfulenchantments/enchantments/HunterEnchantment.java
+++ src/main/java/com/wonderfulenchantments/enchantments/HunterEnchantment.java
@@ -1,13 +1,14 @@
 package com.wonderfulenchantments.enchantments;
 
+import com.mlib.CommonHelper;
 import com.mlib.config.DoubleConfig;
+import com.mlib.math.VectorHelper;
 import com.wonderfulenchantments.Instances;
 import com.wonderfulenchantments.RegistryHandler;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.Arrow;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.living.LootingLevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -22,12 +23,16 @@ public class HunterEnchantment extends WonderfulEnchantment {
 
 	public HunterEnchantment() {
 		super( "hunter", Rarity.RARE, RegistryHandler.BOW_AND_CROSSBOW, EquipmentSlot.MAINHAND, "Hunter" );
-		String damage_comment = "Extra damage multiplier to distance per enchantment level.";
-		String distance_comment = "Minimum required distance to not get any damage penalty.";
-		String penalty_comment = "Maximum damage penalty if entity is very close.";
-		this.damageMultiplier = new DoubleConfig( "damage_multiplier", damage_comment, false, 0.0001, 0.0, 0.01 );
-		this.minimumDistance = new DoubleConfig( "minimum_distance", distance_comment, false, 80.0, 1.0, 10000.0 );
-		this.damagePenaltyMultiplier = new DoubleConfig( "penalty_multiplier", penalty_comment, false, 0.5, 0.0, 1.0 );
+
+		String damageComment = "Extra damage multiplier to distance per enchantment level.";
+		this.damageMultiplier = new DoubleConfig( "damage_multiplier", damageComment, false, 0.0001, 0.0, 0.01 );
+
+		String distanceComment = "Minimum required distance to not get any damage penalty.";
+		this.minimumDistance = new DoubleConfig( "minimum_distance", distanceComment, false, 80.0, 1.0, 10000.0 );
+
+		String penaltyComment = "Maximum damage penalty if a mob is very close.";
+		this.damagePenaltyMultiplier = new DoubleConfig( "penalty_multiplier", penaltyComment, false, 0.5, 0.0, 1.0 );
+
 		this.enchantmentGroup.addConfigs( this.damageMultiplier, this.minimumDistance, this.damagePenaltyMultiplier );
 
 		setMaximumEnchantmentLevel( 3 );
@@ -39,13 +44,10 @@ public class HunterEnchantment extends WonderfulEnchantment {
 	@SubscribeEvent
 	public static void spawnExtraLoot( LootingLevelEvent event ) {
 		DamageSource damageSource = event.getDamageSource();
+		LivingEntity entity = CommonHelper.castIfPossible( LivingEntity.class, damageSource.getEntity() );
 
-		if( !isValid( damageSource ) )
-			return;
-
-		LivingEntity entity = ( LivingEntity )damageSource.getEntity();
-		int hunterLevel = EnchantmentHelper.getItemEnchantmentLevel( Instances.HUNTER, entity.getMainHandItem() );
-		event.setLootingLevel( event.getLootingLevel() + hunterLevel );
+		if( isValid( damageSource ) && entity != null )
+			event.setLootingLevel( event.getLootingLevel() + Instances.HUNTER.getEnchantmentLevel( entity.getMainHandItem() ) );
 	}
 
 	/** Event that increases damage dealt by entity. */
@@ -53,29 +55,24 @@ public class HunterEnchantment extends WonderfulEnchantment {
 	public static void onHit( LivingHurtEvent event ) {
 		DamageSource damageSource = event.getSource();
 		LivingEntity target = event.getEntityLiving();
+		LivingEntity attacker = CommonHelper.castIfPossible( LivingEntity.class, damageSource.getEntity() );
+		HunterEnchantment hunter = Instances.HUNTER;
 
-		if( !isValid( damageSource ) )
-			return;
-
-		LivingEntity attacker = ( LivingEntity )damageSource.getEntity();
-		HunterEnchantment enchantment = Instances.HUNTER;
-		int hunterLevel = EnchantmentHelper.getItemEnchantmentLevel( enchantment, attacker.getMainHandItem() );
-		if( hunterLevel <= 0 )
-			return;
-
-		double distance = attacker.position()
-			.distanceToSqr( target.position() );
-		double penaltyMultiplier = Math.max( 1.0 - distance / enchantment.minimumDistance.get(), 0.0 ) * enchantment.damagePenaltyMultiplier.get();
-		double extraDamageMultiplier = distance * enchantment.damageMultiplier.get() * hunterLevel + 1.0 - penaltyMultiplier;
-		event.setAmount( ( float )( event.getAmount() * extraDamageMultiplier ) );
+		if( isValid( damageSource ) && attacker != null && hunter.hasEnchantment( attacker.getMainHandItem() ) )
+			event.setAmount( ( float )( event.getAmount() * hunter.getDamageMultiplier( attacker, target ) ) );
 	}
 
-	/**
-	 Checking if damage source comes from arrow and is caused (fired) by the entity. (not dispenser for example)
-
-	 @param source Damage source to check.
-	 */
+	/** Checks if a damage source comes from an arrow and is caused (fired) by the entity. (not dispenser for example) */
 	protected static boolean isValid( DamageSource source ) {
 		return source != null && source.getDirectEntity() instanceof Arrow && source.getEntity() instanceof LivingEntity;
+	}
+
+	/** Returns the final damage multiplier. */
+	protected double getDamageMultiplier( LivingEntity attacker, LivingEntity target ) {
+		int hunterLevel = getEnchantmentLevel( attacker.getMainHandItem() );
+		double distance = VectorHelper.distanceSquared( attacker.position(), target.position() );
+		double penaltyMultiplier = Math.max( 1.0 - distance / this.minimumDistance.get(), 0.0 ) * this.damagePenaltyMultiplier.get();
+
+		return distance * this.damageMultiplier.get() * hunterLevel + 1.0 - penaltyMultiplier;
 	}
 }
