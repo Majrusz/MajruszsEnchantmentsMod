af21390bc8c69f2befd2a574be4b43f5d266c2a9
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-09-18 20:52:17 +0200

    Refactoring

--- src/main/java/com/wonderfulenchantments/curses/VampirismCurse.java
+++ src/main/java/com/wonderfulenchantments/curses/VampirismCurse.java
@@ -1,18 +1,18 @@
 package com.wonderfulenchantments.curses;
 
+import com.mlib.CommonHelper;
 import com.mlib.EquipmentSlots;
 import com.mlib.LevelHelper;
 import com.mlib.config.DurationConfig;
 import com.mlib.effects.EffectHelper;
-import com.mlib.enchantments.EnchantmentHelperPlus;
+import com.mlib.time.TimeHelper;
 import com.wonderfulenchantments.Instances;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -21,8 +21,6 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber
 public class VampirismCurse extends WonderfulCurse {
 	private static final MobEffect[] EFFECTS = new MobEffect[]{ MobEffects.WEAKNESS, MobEffects.MOVEMENT_SLOWDOWN, MobEffects.HUNGER };
-	private static final String VAMPIRISM_TAG = "CurseOfVampirismCounter";
-	private static final String VAMPIRISM_PARTICLE_TAG = "CurseOfVampirismParticleCounter";
 	protected final DurationConfig effectDuration;
 	protected final DurationConfig effectCooldown;
 
@@ -45,33 +43,25 @@ public class VampirismCurse extends WonderfulCurse {
 	@SubscribeEvent
 	public static void onUpdate( LivingEvent.LivingUpdateEvent event ) {
 		LivingEntity entity = event.getEntityLiving();
-		if( !( entity.level instanceof ServerLevel ) )
+		ServerLevel level = CommonHelper.castIfPossible( ServerLevel.class, entity.level );
+		if( level == null )
 			return;
 
-		ServerLevel world = ( ServerLevel )entity.level;
 		VampirismCurse vampirism = Instances.VAMPIRISM;
 		int enchantmentLevel = vampirism.getEnchantmentSum( entity.getArmorSlots() );
-		CompoundTag data = entity.getPersistentData();
 
-		int counter = data.getInt( VAMPIRISM_TAG ) + 1;
-		int particleCounter = data.getInt( VAMPIRISM_PARTICLE_TAG ) + 1;
 		if( enchantmentLevel > 0 && LevelHelper.isEntityOutsideDuringTheDay( entity ) ) {
-			int particleCooldown = 9 - Math.min( enchantmentLevel, 4 ) * 2;
-			if( particleCounter > particleCooldown ) {
-				particleCounter -= particleCooldown;
-				world.sendParticles( ParticleTypes.SMOKE, entity.getX(), entity.getY( 0.75 ), entity.getZ(), 1 + enchantmentLevel / 2,
-					0.1, 0.25, 0.1, 0.01
+			if( TimeHelper.hasServerTicksPassed( 4 ) )
+				level.sendParticles( ParticleTypes.SMOKE, entity.getX(), entity.getY( 0.5 ), entity.getZ(), 3 + enchantmentLevel, 0.125, 0.5, 0.125,
+					0.01
 				);
-			}
-			if( counter > vampirism.effectCooldown.getDuration() ) {
-				counter -= vampirism.effectCooldown.getDuration();
+
+			if( TimeHelper.hasServerTicksPassed( vampirism.effectCooldown.getDuration() ) ) {
 				for( MobEffect effect : EFFECTS )
 					EffectHelper.applyEffectIfPossible( entity, effect, vampirism.effectDuration.getDuration() * enchantmentLevel, 0 );
 
 				entity.setSecondsOnFire( 3 + 2 * enchantmentLevel );
 			}
 		}
-		data.putInt( VAMPIRISM_TAG, counter );
-		data.putInt( VAMPIRISM_PARTICLE_TAG, particleCounter );
 	}
 }
