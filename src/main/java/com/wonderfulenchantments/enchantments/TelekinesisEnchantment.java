61a63dd2c8b8e06453503238026b69d9da513712
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-07-23 16:55:18 +0200

    Fixed telekinesis priority

--- src/main/java/com/wonderfulenchantments/enchantments/TelekinesisEnchantment.java
+++ src/main/java/com/wonderfulenchantments/enchantments/TelekinesisEnchantment.java
@@ -2,8 +2,11 @@ package com.wonderfulenchantments.enchantments;
 
 import com.mlib.EquipmentSlots;
 import com.mlib.enchantments.CustomEnchantment;
+import com.mlib.gamemodifiers.Context;
 import com.mlib.gamemodifiers.contexts.OnLootContext;
 import com.mlib.gamemodifiers.data.OnLootData;
+import com.mlib.gamemodifiers.parameters.ContextParameters;
+import com.mlib.gamemodifiers.parameters.Priority;
 import com.mlib.mixininterfaces.IMixinProjectile;
 import com.wonderfulenchantments.Registries;
 import com.wonderfulenchantments.gamemodifiers.EnchantmentModifier;
@@ -26,17 +29,19 @@ public class TelekinesisEnchantment extends CustomEnchantment {
 	}
 
 	private static class Modifier extends EnchantmentModifier< TelekinesisEnchantment > {
+		static final ContextParameters LOWEST_PRIORITY = new ContextParameters( Priority.LOWEST, "", "" );
+
 		public Modifier( TelekinesisEnchantment enchantment ) {
 			super( enchantment, "Telekinesis", "Adds acquired items directly to player's inventory." );
 
-			OnLootContext onLoot = new OnLootContext( data->this.addToInventory( data, data.entity ) );
+			OnLootContext onLoot = new OnLootContext( data->this.addToInventory( data, data.entity ), LOWEST_PRIORITY );
 			onLoot.addCondition( data->data.entity instanceof Player ).addCondition( data->data.tool != null && enchantment.hasEnchantment( data.tool ) );
 
-			OnLootContext onLoot2 = new OnLootContext( data->this.addToInventory( data, data.killer ) );
+			OnLootContext onLoot2 = new OnLootContext( data->this.addToInventory( data, data.killer ), LOWEST_PRIORITY );
 			onLoot2.addCondition( data->data.killer instanceof Player )
 				.addCondition( data->enchantment.hasEnchantment( ( Player )data.killer ) );
 
-			OnLootContext onLoot3 = new OnLootContext( data->this.addToInventory( data, data.killer ) );
+			OnLootContext onLoot3 = new OnLootContext( data->this.addToInventory( data, data.killer ), LOWEST_PRIORITY );
 			onLoot3.addCondition( data->data.killer instanceof Player )
 				.addCondition( data->data.damageSource != null && enchantment.hasEnchantment( IMixinProjectile.getWeaponFromDirectEntity( data.damageSource ) ) );
 
