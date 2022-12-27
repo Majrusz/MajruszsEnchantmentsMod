ac1706e355b5b6b6d7ccdbbf72737a15cd2575e2
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-27 22:27:21 +0100

    Reworked Enlightenment

--- src/main/java/com/majruszsenchantments/enchantments/EnlightenmentEnchantment.java
+++ src/main/java/com/majruszsenchantments/enchantments/EnlightenmentEnchantment.java
@@ -4,6 +4,7 @@ import com.majruszsenchantments.Registries;
 import com.majruszsenchantments.gamemodifiers.EnchantmentModifier;
 import com.mlib.EquipmentSlots;
 import com.mlib.Random;
+import com.mlib.annotations.AutoInstance;
 import com.mlib.config.DoubleConfig;
 import com.mlib.enchantments.CustomEnchantment;
 import com.mlib.gamemodifiers.Condition;
@@ -12,41 +13,38 @@ import com.mlib.gamemodifiers.contexts.OnPickupXp;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
-import java.util.function.Supplier;
-
 public class EnlightenmentEnchantment extends CustomEnchantment {
-	public static Supplier< EnlightenmentEnchantment > create() {
-		Parameters params = new Parameters( Rarity.RARE, EnchantmentCategory.ARMOR, EquipmentSlots.ARMOR, false, 2, level->6 + 12 * level, level->26 + 12 * level );
-		EnlightenmentEnchantment enchantment = new EnlightenmentEnchantment( params );
-		Modifier modifier = new EnlightenmentEnchantment.Modifier( enchantment );
-
-		return ()->enchantment;
+	public EnlightenmentEnchantment() {
+		this.rarity( Rarity.RARE )
+			.category( EnchantmentCategory.ARMOR )
+			.slots( EquipmentSlots.ARMOR )
+			.maxLevel( 2 )
+			.minLevelCost( level->level * 12 + 6 )
+			.maxLevelCost( level->level * 12 + 26 )
+			.setEnabledSupplier( Registries.getEnabledSupplier( Modifier.class ) );
 	}
 
-	public EnlightenmentEnchantment( Parameters params ) {
-		super( params );
-	}
-
-	private static class Modifier extends EnchantmentModifier< EnlightenmentEnchantment > {
+	@AutoInstance
+	public static class Modifier extends EnchantmentModifier< EnlightenmentEnchantment > {
 		final DoubleConfig experienceMultiplier = new DoubleConfig( "experience_extra_multiplier", "Extra percent of experience acquired from all sources per enchantment level.", false, 0.25, 0.01, 10.0 );
 
-		public Modifier( EnlightenmentEnchantment enchantment ) {
-			super( enchantment, "Enlightenment", "Increases the experience gained from any source." );
+		public Modifier() {
+			super( Registries.ENLIGHTENMENT, Registries.Modifiers.ENCHANTMENT, "Enlightenment", "Increases the experience gained from any source." );
 
-			OnPickupXp.Context onXpPickup = new OnPickupXp.Context( this::increaseExperience );
-			onXpPickup.addCondition( new Condition.HasEnchantment<>( enchantment ) );
+			new OnPickupXp.Context( this::increaseExperience )
+				.addCondition( new Condition.HasEnchantment<>( this.enchantment ) )
+				.addConfig( this.experienceMultiplier )
+				.insertTo( this );
 
-			OnEquipmentChanged.Context onEquipmentChanged = new OnEquipmentChanged.Context( this::giveAdvancement );
-			onEquipmentChanged.addCondition( data->data.entity instanceof ServerPlayer )
-				.addCondition( data->enchantment.getEnchantmentSum( data.entity, EquipmentSlots.ARMOR ) == 8 );
-
-			this.addConfig( this.experienceMultiplier );
-			this.addContexts( onXpPickup, onEquipmentChanged );
+			new OnEquipmentChanged.Context( this::giveAdvancement )
+				.addCondition( data->data.entity instanceof ServerPlayer )
+				.addCondition( data->this.enchantment.get().getEnchantmentSum( data.entity, EquipmentSlots.ARMOR ) == 8 );
 		}
 
 		private void increaseExperience( OnPickupXp.Data data ) {
-			int enlightenmentSum = this.enchantment.getEnchantmentSum( data.player, EquipmentSlots.ARMOR );
-			int experiencePoints = Random.roundRandomly( enlightenmentSum * this.experienceMultiplier.get() * data.event.getOrb().getValue() );
+			int enlightenmentSum = this.enchantment.get().getEnchantmentSum( data.player, EquipmentSlots.ARMOR );
+			int experiencePoints = Random.roundRandomly( enlightenmentSum * this.experienceMultiplier.get() * data.event.getOrb()
+				.getValue() );
 			if( experiencePoints > 1 ) {
 				data.player.giveExperiencePoints( Random.nextInt( 1, experiencePoints ) );
 			} else if( experiencePoints > 0 ) {
