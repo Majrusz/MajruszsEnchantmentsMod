b80322fa8491a652c1d90cd96f26583ae11fcac8
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-30 19:18:38 +0100

    Reworked Horse Armored Caravan

--- src/main/java/com/majruszsenchantments/enchantments/HorseProtectionEnchantment.java
+++ src/main/java/com/majruszsenchantments/enchantments/HorseProtectionEnchantment.java
@@ -3,47 +3,47 @@ package com.majruszsenchantments.enchantments;
 import com.majruszsenchantments.Registries;
 import com.majruszsenchantments.gamemodifiers.EnchantmentModifier;
 import com.mlib.EquipmentSlots;
+import com.mlib.annotations.AutoInstance;
 import com.mlib.attributes.AttributeHandler;
 import com.mlib.config.DoubleConfig;
 import com.mlib.enchantments.CustomEnchantment;
 import com.mlib.gamemodifiers.Condition;
 import com.mlib.gamemodifiers.contexts.OnEquipmentChanged;
+import com.mlib.math.Range;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.animal.Animal;
 
-import java.util.function.Supplier;
-
 public class HorseProtectionEnchantment extends CustomEnchantment {
-	public static Supplier< HorseProtectionEnchantment > create() {
-		Parameters params = new Parameters( Rarity.RARE, Registries.HORSE_ARMOR, EquipmentSlots.ARMOR, false, 4, level->-5 + 6 * level, level->5 + 6 * level );
-		HorseProtectionEnchantment enchantment = new HorseProtectionEnchantment( params );
-		Modifier modifier = new HorseProtectionEnchantment.Modifier( enchantment );
-
-		return ()->enchantment;
+	public HorseProtectionEnchantment() {
+		this.rarity( Rarity.RARE )
+			.category( Registries.HORSE_ARMOR )
+			.slots( EquipmentSlots.ARMOR )
+			.maxLevel( 4 )
+			.minLevelCost( level->level * 6 - 5 )
+			.maxLevelCost( level->level * 6 + 5 )
+			.setEnabledSupplier( Registries.getEnabledSupplier( Modifier.class ) );
 	}
 
-	public HorseProtectionEnchantment( Parameters params ) {
-		super( params );
-	}
-
-	private static class Modifier extends EnchantmentModifier< HorseProtectionEnchantment > {
+	@AutoInstance
+	public static class Modifier extends EnchantmentModifier< HorseProtectionEnchantment > {
 		static final AttributeHandler ARMOR_ATTRIBUTE = new AttributeHandler( "f7f6f46b-23a1-4d3b-8e83-3160c6390f9a", "HorseProtectionBonus", Attributes.ARMOR, AttributeModifier.Operation.ADDITION );
-		final DoubleConfig armorBonus = new DoubleConfig( "armor_bonus", "Armor bonus per enchantment level.", false, 2.0, 1.0, 10.0 );
+		final DoubleConfig armorBonus = new DoubleConfig( 2.0, new Range<>( 1.0, 10.0 ) );
 
-		public Modifier( HorseProtectionEnchantment enchantment ) {
-			super( enchantment, "ArmoredCaravan", "Increases the horse's armor." );
+		public Modifier() {
+			super( Registries.HORSE_PROTECTION, Registries.Modifiers.ENCHANTMENT );
 
-			OnEquipmentChanged.Context onChange = new OnEquipmentChanged.Context( this::updateSpeed );
-			onChange.addCondition( new Condition.IsServer<>() )
+			new OnEquipmentChanged.Context( this::updateSpeed )
+				.addCondition( new Condition.IsServer<>() )
 				.addCondition( data->data.entity instanceof Animal ) // checks for the animal class instead of horse to have a compatibility with other mods
-				.addConfig( this.armorBonus );
+				.addConfig( this.armorBonus.name( "armor_bonus" ).comment( "Armor bonus per enchantment level." ) )
+				.insertTo( this );
 
-			this.addContext( onChange );
+			this.name( "ArmoredCaravan" ).comment( "Increases the horse's armor." );
 		}
 
 		private void updateSpeed( OnEquipmentChanged.Data data ) {
-			float extraArmor = this.armorBonus.asFloat() * this.enchantment.getEnchantmentSum( data.entity, EquipmentSlots.ARMOR );
+			float extraArmor = this.armorBonus.asFloat() * this.enchantment.get().getEnchantmentSum( data.entity, EquipmentSlots.ARMOR );
 			ARMOR_ATTRIBUTE.setValueAndApply( data.entity, extraArmor );
 		}
 	}
