4234a1742daadfbc58536ba3e784ec9abb40ce28
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-31 21:24:06 +0100

    Made the glow be only visible on the client side

--- src/main/java/com/majruszsenchantments/enchantments/SixthSenseEnchantment.java
+++ src/main/java/com/majruszsenchantments/enchantments/SixthSenseEnchantment.java
@@ -3,15 +3,15 @@ package com.majruszsenchantments.enchantments;
 import com.majruszsenchantments.Registries;
 import com.majruszsenchantments.gamemodifiers.EnchantmentModifier;
 import com.mlib.EquipmentSlots;
-import com.mlib.Utility;
 import com.mlib.annotations.AutoInstance;
+import com.mlib.config.DoubleConfig;
 import com.mlib.enchantments.CustomEnchantment;
+import com.mlib.entities.EntityHelper;
 import com.mlib.gamemodifiers.Condition;
 import com.mlib.gamemodifiers.contexts.OnEntitySignalCheck;
 import com.mlib.gamemodifiers.contexts.OnEntitySignalReceived;
-import com.mlib.mobeffects.MobEffectHelper;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.LivingEntity;
+import com.mlib.math.Range;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
 public class SixthSenseEnchantment extends CustomEnchantment {
@@ -26,6 +26,8 @@ public class SixthSenseEnchantment extends CustomEnchantment {
 
 	@AutoInstance
 	public static class Modifier extends EnchantmentModifier< SixthSenseEnchantment > {
+		final DoubleConfig glowDuration = new DoubleConfig( 2.0, new Range<>( 0.5, 15.0 ) );
+
 		public Modifier() {
 			super( Registries.SIXTH_SENSE, Registries.Modifiers.ENCHANTMENT );
 
@@ -35,14 +37,17 @@ public class SixthSenseEnchantment extends CustomEnchantment {
 				.insertTo( this );
 
 			new OnEntitySignalReceived.Context( this::highlightEntity )
-				.addCondition( data->data.owner instanceof LivingEntity )
+				.addCondition( data->data.player instanceof ServerPlayer )
+				.addCondition( data->data.owner != null )
+				.addConfig( this.glowDuration.name( "glow_duration" )
+					.comment( "Determines how long the mob should be highlighted (this value stacks up with every sound emitted)." ) )
 				.insertTo( this );
 
 			this.name( "SixthSense" ).comment( "Adds acquired items directly to player's inventory." );
 		}
 
 		private void highlightEntity( OnEntitySignalReceived.Data data ) {
-			MobEffectHelper.tryToApply( ( LivingEntity )data.owner, MobEffects.GLOWING, Utility.secondsToTicks( 5.0 ), 0 );
+			EntityHelper.sendExtraClientGlowTicks( ( ServerPlayer )data.player, data.owner, this.glowDuration.asTicks() );
 		}
 	}
 }
